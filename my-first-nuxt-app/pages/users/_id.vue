d6841151a169bb7a5f9c8cc73eb1c74657ee8eee
usmy <usa12yu18@gmail.com>
2019-11-25 17:18:38 +0900

    _id > _userid

--- my-first-nuxt-app/pages/users/_id.vue
+++
@@ -1,5 +0,0 @@
-<template id="">
-  <div class="">
-    <p>/users/_id.vue</p>
-  </div>
-</template>
---
+++ my-first-nuxt-app/pages/users/_userid.vue
@@ -0,0 +1,15 @@
+<template id="">
+  <div class="">
+    <p>User ID: {{userID}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        userID: this.$route.params.id
+      }
+    }
+  }
+</script>
