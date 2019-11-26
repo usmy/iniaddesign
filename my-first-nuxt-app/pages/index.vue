777ce2004d8d98bedf7b318a9d5f98a8f9457605
usmy <usa12yu18@gmail.com>
2019-11-26 13:27:35 +0900

    ユーザー情報の表示

--- my-first-nuxt-app/pages/index.vue
+++ my-first-nuxt-app/pages/index.vue
@@ -6,7 +6,12 @@
         <li v-for="item in items" :key="item.id">
           <h4>
             <span>{{item.title}} </span>
-            <small> by {{item.user.id}}</small>
+            <small>
+              <span>by </span>
+              <nuxt-link :to="`/users/${item.user.id}`">
+                {{item.user.id}}
+              </nuxt-link>
+            </small>
           </h4>
           <div class="">{{item.body.slice(0, 130)}}……</div>
           <p><a :href="item.url">{{item.url}}</a></p>
@@ -19,7 +24,7 @@
 <script>
 export default {
   async asyncData({ app }) {
-    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:Vue.js')
+    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
     return {
       items
     }
--- my-first-nuxt-app/pages/users/_userid.vue
+++ my-first-nuxt-app/pages/users/_userid.vue
@@ -1,15 +1,54 @@
 <template id="">
-  <div class="">
-    <p>User ID: {{userID}}</p>
-  </div>
+  <section class="container">
+    <h3>{{user.id}}</h3>
+    <img :src="user.profile_image_url" width="120" alt="">
+    <p>{{user.description || 'No description'}}</p>
+    <p>
+      <nuxt-link to="/">
+        <small><b>トップへ戻る</b></small>
+      </nuxt-link>
+    </p>
+    <h3>{{user.id}}さんの投稿一覧</h3>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        <h4>
+          <span>{{item.title}}</span>
+        </h4>
+        <div class="">{{item.body.slice(0, 130)}}……</div>
+        <p><a target="_blank" :href="item.url">{{item.url}}</a></p>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
   export default {
-    data() {
-      return {
-        userID: this.$route.params.id
-      }
+    async asyncData({ route, app }) {
+      const user = await app.$axios.$get(`https://qiita.com/api/v2/users/${route.params.userid}`)
+      const items = await app.$axios.$get(`https://qiita.com/api/v2/items?query=user:${route.params.userid}`)
+      return { user, items }
     }
   }
 </script>
+
+<style>
+  .container {
+    min-height: 100vh;
+    padding: 16px;
+  }
+
+  h3 {
+    margin: 16px 0;
+    padding: 8px 0;
+    border-bottom: solid 1px #e5e5e5;
+  }
+
+  li + li {
+    margin: 16px 0;
+  }
+
+  p {
+    margin: 8px 0;
+  }
+
+</style>
